6588ca1dd912ec54bd79f7727eb68df72d4585a2
Yunting Chiu <58647044+twyunting@users.noreply.github.com>
2020-05-27 00:03:16 +0800

    Add files via upload

---
+++ AdventureSwift_Working/Ch2_Variable_Basics.playground/Pages/Swift operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+/*:
+ # Swift Operators
+ ---
+ 
+ ## Topic Essentials
+ Operators in Swift are used to create mathematical expression, evaluate equality, and declare value ranges.
+ 
+ ### Objectives
+ + Understand the different operators and their basic uses
+ 
+ [Previous Topic](@previous)                                                 [Next Topic](@next)
+ 
+ */
+// Arithmetic (+, -, /, *)
+var usingMathinswift = 5 + (4 - 3) / 2 * 1
+
+// Compound assignments (+=, -=, *=, /=, %=)
+var HelloWorld = 55688
+HelloWorld *= 12
+HelloWorld /= 50
+
+// Modulo (%)
+
+var experienceOverflow = 100 % 90
+print(experienceOverflow)
+
+/*  Comparisons (==, !=, >, <, >=, <=)
+    Logical operators (!, &&, ||)
+    Ranges (1...5 or 1..<5)
+*/
